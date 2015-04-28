9c2a2277a994582cade86a6d5b581e2a790982c9
Mitesh Shah <Mr.Miteshah@gmail.com>
2015-04-28 17:15:19 +0530

    Update macosx.sh

--- distro/macosx.sh
+++ distro/macosx.sh
@@ -8,3 +8,7 @@ brew install caskroom/cask/brew-cask
 
 echo "Installing Google Chrome, please wait..."
 brew cask install google-chrome
+brew cask install 1password
+
+echo "Installing Oh-My-ZSH, please wait..."
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
