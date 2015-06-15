2661f30a98c0c53551dce96712d68df08f6a5534
Mitesh Shah <Mr.Miteshah@gmail.com>
2015-06-15 16:47:06 +0530

    Update desktop-setup.sh

--- distro/ubuntu/desktop-setup.sh
+++ distro/ubuntu/desktop-setup.sh
@@ -32,11 +32,15 @@ echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
   wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add - \
   || ee_lib_error "Unable to add Hipchat repository, exit status = " $?
 
+# Caffeine
+sudo add-apt-repository ppa:caffeine-developers/ppa \
+|| ee_lib_error "Unable to add caffeine repository, exit status = " $?
+
 # Execute: apt-get update
 apt-get update \
 || error "Unable to execute apt-get update command, exit status = " $?
 
 # Install required packages
 echo "Installing necessary packages, please wait..."
-apt-get -y install git-core openssh-server shutter google-chrome-stable vlc hipchat openjdk-8-jre openjdk-8-jdk gparted diodon nautilus-open-terminal \
+apt-get -y install git-core openssh-server shutter google-chrome-stable vlc hipchat openjdk-8-jre openjdk-8-jdk gparted diodon caffeine nautilus-open-terminal \
 || error "Unable to install required packages, exit status = " $?
