bac16759d7ca436cbb2239a8210db3849c61d542
Mitesh Shah <Mr.Miteshah@gmail.com>
2015-06-17 18:28:56 +0530

    Update desktop-setup.sh

--- distro/ubuntu/desktop-setup.sh
+++ distro/ubuntu/desktop-setup.sh
@@ -31,6 +31,11 @@ echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
   /etc/apt/sources.list.d/atlassian-hipchat.list && \
   wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add - \
   || ee_lib_error "Unable to add Hipchat repository, exit status = " $?
+  
+# Skype Repository 		
+echo "Adding skype repository, please wait..."		
+sudo sh -c  'echo "deb http://archive.canonical.com/ubuntu/ $(lsb_release -sc) partner" >> /etc/apt/sources.list.d/canonical_partner.list' \		
+|| ee_lib_error "Unable to add skype repository, exit status = " $?
 
 # Caffeine
 sudo add-apt-repository ppa:caffeine-developers/ppa \
@@ -42,5 +47,5 @@ apt-get update \
 
 # Install required packages
 echo "Installing necessary packages, please wait..."
-apt-get -y install git-core openssh-server shutter google-chrome-stable vlc hipchat openjdk-8-jre openjdk-8-jdk gparted diodon caffeine nautilus-open-terminal \
+apt-get -y install git-core openssh-server shutter google-chrome-stable vlc hipchat skype sni-qt sni-qt:i386 openjdk-8-jre openjdk-8-jdk gparted diodon caffeine nautilus-open-terminal \
 || error "Unable to install required packages, exit status = " $?
