a28fb3ff4042e2947da266f7ea1ab95bd540ed1e
Mitesh Shah <Mr.Miteshah@gmail.com>
2015-07-06 11:22:51 +0530

    Update macosx.sh

--- Mac/macosx.sh
+++ Mac/macosx.sh
@@ -6,9 +6,27 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 echo "Installing HomeBrew Cask, please wait..."
 brew install caskroom/cask/brew-cask
 
+echo "Installing ssh-copy-id command, please wait..."
+brew install ssh-copy-id
+
+echo "Installing Watch command, please wait..."
+brew install watch
+
 echo "Installing Google Chrome, please wait..."
 brew cask install google-chrome
 brew cask install 1password
+
+echo "Installing VLC, please wait..."
+brew cask install vlc
+
+echo "Installing Skitch, please wait..."
+brew cask install skitch
+
+echo "Installing Caffeine, please wait..."
+brew cask install caffeine
+
+echo "Installing Atom, please wait..."
+brew cask install atom
 
 echo "Installing Vagrant, please wait..."
 brew cask install vagrant
@@ -18,18 +36,3 @@ brew cask install virtualbox
 
 echo "Installing Oh-My-ZSH, please wait..."
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
-echo "Installing Atom, please wait..."
-brew cask install atom
-
-echo "Installing VLC, please wait..."
-brew cask install vlc
-
-echo "Installing Caffeine, please wait..."
-brew cask install caffeine
-
-echo "Installing ssh-copy-id command, please wait..."
-brew install ssh-copy-id
-
-echo "Installing Watch command, please wait..."
-brew install watch
